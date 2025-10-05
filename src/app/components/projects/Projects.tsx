9ff0f6529b44d277f43d8f81845d9ba790f48313
minhajzein <minhajzein@gmail.com>
2025-10-05 21:50:08 +0530

    carousel added in projects section in landng page

--- src/app/components/projects/Projects.tsx
+++ src/app/components/projects/Projects.tsx
@@ -1,12 +1,32 @@
 import projects from '@/lib/constants/projects'
 import ProjectCard from './ProjectCard'
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/ui/carousel'
 
 function Projects() {
 	return (
-		<div className='grid md:grid-cols-3 gap-[18px]'>
-			{projects.slice(0, 3).map((project, i) => (
-				<ProjectCard key={`${project.title}-${i}`} project={project} />
-			))}
+		<div className='flex w-full flex-col '>
+			<Carousel className='w-full relative'>
+				<CarouselContent>
+					{projects.map((project, i) => (
+						<CarouselItem
+							key={`${project.title}-${i}`}
+							className='md:basis-1/2 lg:basis-1/3'
+						>
+							<ProjectCard project={project} />
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<div className='absolute top-full translate-y-full right-12 flex gap-2 pt-4'>
+					<CarouselPrevious />
+					<CarouselNext />
+				</div>
+			</Carousel>
 		</div>
 	)
 }
